b120e8eebf240853ce1ac02fc0c447bcabb6db0c
Daniel Cruz <danielc@unb.br>
2025-06-08 21:42:07 -0300

    Implementando registro de concluído e filtros

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import datetime
 import os
+import sys
 
-arquivo="todo.txt"
+ARQUIVO="todo.txt"
 
 
-def adicionar(info, tags='all'):
+def adicionar(info, n, tags='all'):
     yyyy = str(datetime.datetime.today().year).zfill(2)
     mm = str(datetime.datetime.today().month).zfill(2)
     dd = str(datetime.datetime.today().day).zfill(2)
@@ -12,41 +13,143 @@ def adicionar(info, tags='all'):
     m = str(datetime.datetime.today().minute).zfill(2)
     s = str(datetime.datetime.today().second).zfill(2)
 
-    with open(arquivo, 'a') as f:
-        f.write(f'[{yyyy}-{mm}-{dd}_{h}:{m}:{s}][*{tags}*][ ]{info}\n')
+    with open(ARQUIVO, 'a') as f:
+        f.write(f'{n}--[{yyyy}-{mm}-{dd}_{h}:{m}:{s}][*{tags}*][ ]{info}\n')
 
 
+def filtrar_registros(linhas, filtro=False):
+    if not filtro:
+        linhas_ver = [l for l in linhas]
+    elif filtro == 'ld':
+        linhas_ver = list(filter(lambda l: True if l.find('[ ]') == -1 else False, linhas))
+    elif filtro == 'lp':
+        linhas_ver = list(filter(lambda l: True if l.find('[x]') == -1 else False, linhas))
 
-def ler():
+    return linhas_ver
+
+
+def contar_linhas():
     linhas = None
-    with open(arquivo) as f:
-        linhas = f.read().split('\n')
-    
-    linhas_ver = [l for l in linhas]
-        
+    try:
+        with open(ARQUIVO) as f:
+            linhas = f.read().split('\n')
+    except FileNotFoundError:
+        return 0
+    return len(tuple(filter(None, linhas)))
+
+
+def ler(filtro=False):
+    linhas = None
+    no_info=False
+    try:
+        with open(ARQUIVO) as f:
+            linhas = f.read().split('\n')
+    except FileNotFoundError:
+        no_info = True
+
+    linhas_ver = filtrar_registros(linhas, filtro=filtro)
+       
     print("\033[2J", end="")  # Limpa toda a tela
     print("")
-    print(*linhas_ver, sep='\n')
+    if no_info:
+        print("\nSem registros...", sep='\n')
+    else:
+        for l in linhas_ver:
+            try:
+                idx = int(l.split('--')[0])+1
+                print(f"{str(idx).zfill(2)}: [{l.split('*][')[1]}")
+            except IndexError:
+                pass
+            except ValueError:
+                pass
+
+        #linhas_ver = ['['+l.split('*][')[1] for l in linhas_ver]
+        #print(*linhas_ver, sep='\n')
     print("\033[H", end="")   # Coloca cursor no topo
 
 
+def concluida(lista_numeros, filtro=False):
+    nums = lista_numeros.strip().split(',')
+    nums = map(int, filter(None, nums))
+    nums = set(map(lambda x: x-1, nums))
+    
+    with open(ARQUIVO) as f:
+        linhas = f.read().split('\n')
+    
+    linhas_ver = filtrar_registros(linhas, filtro=filtro)
+
+    for i, l in enumerate(linhas_ver):
+        if i in nums:
+            idx = int(l.split('--')[0])
+            linhas[idx] = linhas[idx].replace('[ ]', '[x]')
+
+
+    with open(ARQUIVO, 'w') as f:
+        f.write('\n'.join(linhas))
+
+
+def nao_concluida(lista_numeros, filtro=False):
+    nums = lista_numeros.strip().split(',')
+    nums = map(int, filter(None, nums))
+    nums = set(map(lambda x: x-1, nums))
+    with open(ARQUIVO) as f:
+        linhas = f.read().split('\n')
+
+    linhas_ver = filtrar_registros(linhas, filtro=filtro)
+
+    for i, l in enumerate(linhas_ver):
+        if i in nums:
+            idx = int(l.split('--')[0])
+            linhas[idx] = linhas[idx].replace('[x]', '[ ]')
+
+    with open(ARQUIVO, 'w') as f:
+        f.write('\n'.join(linhas))
+
+
+def sair():
+    if sys.platform == 'linux': os.system('clear')
+    else: os.system('cls')
+    print("\nFinalizando programa...\n")
+   
 
 
 def main():
-    os.system('cls')
+    if sys.platform == 'linux': os.system('clear')
+    else: os.system('cls')
+
+    filtro=False
+    n=0
+
     while True:
-        cmd_args = input('$: ').split()
-        
+        n=contar_linhas()
+        ler(filtro)
         try:
+            cmd_args = input('$: ').split()
             if cmd_args[0] == 'a':
-                info = ' '.join(cmd_args[1:])
-                adicionar(info)
+                info = ' '.join(filter(None, cmd_args[1:]))
+                adicionar(info, n)
+            elif cmd_args[0] == 'q':
+                sair()
+                break
+            elif cmd_args[0] == 'd':
+                info = ''.join(cmd_args[1:])
+                concluida(info)
+            elif cmd_args[0] == 'u':
+                info = ''.join(cmd_args[1:])
+                nao_concluida(info)
+            elif cmd_args[0] == 'ld':
+                filtro='ld'
+            elif cmd_args[0] == 'lp':
+                filtro='lp'
+            elif cmd_args[0] == 'la':
+                filtro=False
             else:
                 continue
-        except:
+        except IndexError:
             pass
-
-
+        except KeyboardInterrupt:
+            sair()
+            break
 
 
 
